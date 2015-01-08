12c4eb823d279c32ea520495b043b92600409241
xanthos@ntua <xanthos@mail.ntua.gr>
2015-01-08 15:35:39 +0200

    trying to compile the dehanttideinel dir

---
+++ cpp/dehanttideinel.cpp
@@ -0,0 +1,247 @@
+#include "iers2010.hpp"
+#include "dehanttideinel/dehanttideinel.hpp"
+
+/**
+ * @details This function computes the station tidal displacement
+ *          caused by lunar and solar gravitational attraction (see References). 
+ *          The computations are calculated by the following steps:<br>
+ *          <b>Step 1):</b> General degree 2 and degree 3 corrections + CALL ST1IDIU 
+ *          + CALL ST1ISEM + CALL ST1L1.<br>  
+ *          <b>Step 2):</b> CALL STEP2DIU + CALL STEP2LON<br>
+ *          It has been decided that the <b>Step 3</b> non-correction for permanent tide
+ *          would not be applied in order to avoid a jump in the reference frame.
+ *          This Step 3 must be added in order to get the non-tidal station position
+ *          and to conform with the IAG Resolution.
+ *          This function is a translation/wrapper for the fortran DEHANTTIDEINEL
+ *          subroutine, found here : http://maia.usno.navy.mil/conv2010/software.html
+ * 
+ * @param[in]  xsta   Geocentric position of the IGS station (Note 1)
+ * @param[in]  xsun   Geocentric position of the Sun (Note 2)
+ * @param[in]  xmon   Geocentric position of the Moon (Note 2)
+ * @param[in]  jc1    Julian Centuries in TT (Note 6)
+ * @param[in]  jc2    Julian Centuries in TT (Note 6)
+ * @param[out] dxtide Displacement vector (Note 5) 
+ * 
+ * @note
+ *     -# The IGS station is in ITRF co-rotating frame.  All coordinates,
+ *        X, Y, and Z, are expressed in meters. 
+ *     -# The position is in Earth Centered Earth Fixed (ECEF) frame.  All
+ *        coordinates are expressed in meters.
+ *     -# (\b OBSOLETE) The values are expressed in Coordinated Universal Time (UTC).
+ *     -# (\b OBSOLETE) The fractional hours in the day is computed as the hour + minutes/60.0
+ *        + sec/3600.0.
+ *     -# The displacement vector is in the geocentric ITRF.  All components are
+ *        expressed in meters.
+ *     -# Parameters jc1 and jc2 constitute the date as Julian Centuries in TT time
+ *        scale. The actual date is given by the addition jc1+jc2. Either jc1 or jc2
+ *        can be set to zero.
+ *     -# Status: Class 1
+ *     -# This fucnction is part of the package dehanttideinel, see
+ *        ftp://maia.usno.navy.mil/conv2010/convupdt/chapter7/dehanttideinel/ 
+ * 
+ *  Test case:
+ *     given input: XSTA(1) = 4075578.385D0 meters
+ *                  XSTA(2) =  931852.890D0 meters
+ *                  XSTA(3) = 4801570.154D0 meters   
+ *                  XSUN(1) = 137859926952.015D0 meters
+ *                  XSUN(2) = 54228127881.4350D0 meters
+ *                  XSUN(3) = 23509422341.6960D0 meters
+ *                  XMON(1) = -179996231.920342D0 meters
+ *                  XMON(2) = -312468450.131567D0 meters
+ *                  XMON(3) = -169288918.592160D0 meters
+ *                  YR      = 2009
+ *                  MONTH   = 4
+ *                  DAY     = 13
+ *                  FHR     = 0.00D0 hour
+ *                  
+ *     expected output:  DXTIDE(1) = 0.7700420357108125891D-01 meters
+ *                       DXTIDE(2) = 0.6304056321824967613D-01 meters
+ *                       DXTIDE(3) = 0.5516568152597246810D-01 meters
+ *
+ *  Test case:
+ *     given input: XSTA(1) =  1112189.660D0 meters
+ *                  XSTA(2) = -4842955.026D0 meters
+ *                  XSTA(3) =  3985352.284D0 meters   
+ *                  XSUN(1) = -54537460436.2357D0 meters
+ *                  XSUN(2) =  130244288385.279D0 meters
+ *                  XSUN(3) =  56463429031.5996D0 meters
+ *                  XMON(1) =  300396716.912D0 meters
+ *                  XMON(2) =  243238281.451D0 meters
+ *                  XMON(3) =  120548075.939D0 meters
+ *                  YR      = 2012
+ *                  MONTH   = 7
+ *                  DAY     = 13
+ *                  FHR     = 0.00D0 hour
+ *                  
+ *     expected output:  DXTIDE(1) = -0.2036831479592075833D-01 meters
+ *                       DXTIDE(2) =  0.5658254776225972449D-01 meters
+ *                       DXTIDE(3) = -0.7597679676871742227D-01 meters
+ *
+ * @version 2013 May      17
+ *
+ * @cite iers2010,
+ *
+ *     Groten, E., 2000, Geodesists Handbook 2000, Part 4,
+ *     http://www.gfy.ku.dk/~iag/HB2000/part4/groten.htm. See also
+ *     ''Parameters of Common Relevance of Astronomy, Geodesy, and
+ *     Geodynamics," J. Geod., 74, pp. 134-140
+ *
+ *     Mathews, P. M., Dehant, V., and Gipson, J. M., 1997, ''Tidal station
+ *     displacements," J. Geophys. Res., 102(B9), pp. 20,469-20,477
+ *
+ *     Petit, G. and Luzum, B. (eds.), IERS Conventions (2010),
+ *     IERS Technical Note No. 36, BKG (2010)
+ *     
+ *     Pitjeva, E. and Standish, E. M., 2009, ''Proposals for the masses
+ *     of the three largest asteroids, the Moon-Earth mass ratio and the
+ *     Astronomical Unit," Celest. Mech. Dyn. Astr., 103, pp. 365-372
+ *
+ *     Ries, J. C., Eanes, R. J., Shum, C. K. and Watkins, M. M., 1992,
+ *     ''Progress in the Determination of the Gravitational Coefficient
+ *     of the Earth," Geophys. Res. Lett., 19(6), pp. 529-531
+ * 
+ */
+int iers2010::dehanttideinel (const double* xsta, const double* xsun, const double* xmon, const double& jc1, const double& jc2, 
+                            double* dxtide)
+{
+
+  xcorsta[0] = xcorsta[1] = xcorsta[2] = .0e0;
+
+  // Set constants
+  #ifdef USE_EXTERNAL_CONSTS
+    constexpr double PI   (DPI);                              // pi
+  #else
+    constexpr double PI   ( 3.1415926535897932384626433e0 );  // pi
+  #endif
+
+  /*----------------------------------------------------------------------  
+   * NOMINAL SECOND DEGREE AND THIRD DEGREE LOVE NUMBERS AND SHIDA NUMBERS  
+   *----------------------------------------------------------------------*/
+  constexpr double h20 (0.6078e0), l20 (0.0847e0), h3 (0.292e0), l3 (0.015e0);
+
+  /*----------------------------------------------------------------------  
+   * SCALAR PRODUCT OF STATION VECTOR WITH SUN/MOON VECTOR  
+   *----------------------------------------------------------------------*/
+  double rsta,rsun,rmon;
+  double scs = iers2010::dtel::_sprod_ (xsta,xsun,rsta,rsun);
+  double scm = iers2010::dtel::_sprod_ (xsta,xmon,rsta,rmon); 
+  double scsun = scs / rsta / rsun; 
+  double scmon = scm / rsta / rmon;
+
+  /*----------------------------------------------------------------------   
+   * COMPUTATION OF NEW H2 AND L2  
+   *----------------------------------------------------------------------*/
+  double cosphi = ::sqrt ( xsta[0]*xsta[0]+xsta[1]*xsta[1] ) / rsta;
+  double h2     = h20 - 0.0006d0*(1e0-3e0/2e0*cosphi*cosphi);
+  double l2     = l20 + 0.0002e0*(1e0-3e0/2e0*cosphi*cosphi);
+
+  // P2 term  
+  double p2sun = 3e0*(h2/2e0-l2)*scsun*scsun-h2/2e0;
+  double p2mon = 3e0*(h2/2e0-l2)*scmon*scmon-h2/2e0;
+
+  // P3 term  
+  double p3sun = 5e0/2e0*(h3-3e0*l3)*pow(scsun,3)+3e0/2e0*(l3-h3)*scsun;
+  double p3mon = 5e0/2e0*(h3-3e0*l3)*pow(scmon,3)+3e0/2e0*(l3-h3)*scmon;
+
+  /*----------------------------------------------------------------------  
+   * TERM IN DIRECTION OF SUN/MOON VECTOR  
+   *----------------------------------------------------------------------*/
+  double x2sun = 3e0*l2*scsun;
+  double x2mon = 3e0*l2*scmon;
+  double x3sun = 3e0*l3/2e0*(5e0*scsun*scsun-1e0);
+  double x3mon = 3e0*l3/2e0*(5e0*scmon*scmon-1e0);
+
+  /*----------------------------------------------------------------------  
+   * FACTORS FOR SUN/MOON USING IAU CURRENT BEST ESTIMATES (SEE REFERENCES) 
+   *----------------------------------------------------------------------*/
+  const double mass_ratio_sun  = 332946.0482e0;
+  const double mass_ratio_moon = 0.0123000371e0;
+  const double re              = 6378136.6e0;
+  const double fac2sun         = mass_ratio_sun*re*pow(re/rsun,3);
+  const double fac2mon         = mass_ratio_moon*re*pow(re/rmon,3);
+  const double fac3sun         = fac2sun*(re/rsun);
+  const double fac3mon         = fac2mon*(re/rmon);
+  
+  // TOTAL DISPLACEMENT
+  double dxtide[] = {.0e0,.0e0,.0e0};
+  for (int i=0;i<3;i++) 
+    dxtide[i] = fac2sun*( x2sun*xsun[i]/rsun + p2sun*xsta[i]/rsta ) +
+                fac2mon*( x2mon*xmon[i]/rmon + p2mon*xsta[i]/rsta ) +  
+                fac3sun*( x3sun*xsun[i]/rsun + p3sun*xsta[i]/rsta ) +   
+                fac3mon*( x3mon*xmon[i]/rmon + p3mon*xsta[i]/rsta );
+
+  /*+---------------------------------------------------------------------  
+   * CORRECTIONS FOR THE OUT-OF-PHASE PART OF LOVE NUMBERS (PART H_2^(0)I  
+   * AND L_2^(0)I )  
+   *----------------------------------------------------------------------*/
+
+  // FIRST, FOR THE DIURNAL BAND
+  iers2010::dtel::st1idiu ( xsta,xsun,xmon,fac2sun,fac2mon,xcorsta );
+  for (int i=0;i<3;i++) dxtide[i] += xcorsta[i];
+  
+  // SECOND, FOR THE SEMI-DIURNAL BAND       
+  iers2010::dtel::st1isem ( xsta,xsun,xmon,fac2sun,fac2mon,xcorsta );
+  for (int i=0;i<3;i++) dxtide[i] += xcorsta[i];  
+
+
+  /*+---------------------------------------------------------------------
+   * CORRECTIONS FOR THE LATITUDE DEPENDENCE OF LOVE NUMBERS (PART L^(1) )  
+   *----------------------------------------------------------------------*/
+  iers2010::dtel::st1l1 ( xsta,xsun,xmon,fac2sun,fac2mon,xcorsta );
+  for (int i=0;i<3;i++) dxtide[i] += xcorsta[i];    
+  
+  // CONSIDER CORRECTIONS FOR STEP 2
+
+  /*+---------------------------------------------------------------------  
+   * CORRECTIONS FOR THE DIURNAL BAND:  
+   * 
+   *  FIRST, WE NEED TO KNOW THE DATE CONVERTED IN JULIAN CENTURIES 
+   *        
+   *   1) CALL THE SUBROUTINE COMPUTING THE JULIAN DATE 
+   *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/ 
+   ///      CALL CAL2JD ( YR, MONTH, DAY, JJM0, JJM1, STATUT )
+   ///      FHRD = FHR/24.D0
+  /*    17 May 2013 Corrected bug as noted in header                     */
+   ///      T=((JJM0-2451545.0D0)+JJM1+FHRD)/36525D0
+  /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+   *   2) CALL THE SUBROUTINE COMPUTING THE CORRECTION OF UTC TIME  
+   *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+   ///     CALL DAT ( YR, MONTH, DAY, FHRD, DTT, STATUT )
+  /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+   ///      DTT = DTT + 32.184D0
+  /*    CONVERSION OF T IN TT TIME                                        */
+   ///     T=T+DTT/(3600.0D0*24.0D0*36525D0)
+   
+   double t = jc1 + jc2;
+   double fhr = .0e0;
+
+  //  SECOND, WE CAN CALL THE SUBROUTINE STEP2DIU, FOR THE DIURNAL BAND
+  //+ CORRECTIONS, (in-phase and out-of-phase frequency dependence):
+  iers2010::dtel::step2diu ( xsta,fhr,t,xcorsta );
+  for (int i=0;i<3;i++) dxtide[i] += xcorsta[i]; 
+  
+  //  CORRECTIONS FOR THE LONG-PERIOD BAND,
+  //+ (in-phase and out-of-phase frequency dependence):  
+  iers2010::dtel::step2lon ( xsta,t,xcorsta );
+  for (int i=0;i<3;i++) dxtide[i] += xcorsta[i];  
+    
+  // CONSIDER CORRECTIONS FOR STEP 3
+
+  /*----------------------------------------------------------------------
+   * UNCORRECT FOR THE PERMANENT TIDE  
+   *-----------------------------------------------------------------------*/
+/*
+  double sinphi = xsta[2]/rsta;
+  double cosphi = sqrt( xsta[0]*xsta[0]+xsta[1]*xsta[1] ) / rsta;
+  double cosla  = xsta[0]/cosphi/rsta;
+  double sinla  = xsta[1]/cosphi/rsta;
+  double dr     = -sqrt(5e0/4e0/PI)*h2*0.31460e0*(3e0/2e0*sinphi*sinphi-0.5e0);
+  double dn     = -sqrt(5e0/4e0/PI)*l2*0.31460e0*3e0*cosphi*sinphi;
+  dxtide[0]    += -dr*cosla*cosphi+dn*cosla*sinphi;
+  dxtide[1]    += -dr*sinla*cosphi+dn*sinla*sinphi;
+  dxtide[2]    += -dr*sinphi      -dn*cosphi;
+*/
+
+  // Finished.
+  return 0;
+}
---
+++ cpp/dehanttideinel/dehanttideinel.hpp
@@ -0,0 +1,77 @@
+#ifndef _IERS_1010_DEHANTTIDEINEL_
+#define _IERS_1010_DEHANTTIDEINEL_
+
+/**
+ * @file      dehanttideinel.hpp
+ * 
+ * @version   1.0-1b
+ * 
+ * @author    xanthos@mail.ntua.gr
+ *            danast@mail.ntua.gr
+ * 
+ * @date      January, 2015
+ *
+ * @brief     Additional C++ functions and definitions implementing the IERS 2010 standards; This
+ *            header file accompanies the dehanttideinel software.  
+ *
+ * @note      
+ *         -# Original FORTRAN software can be found at http://maia.usno.navy.mil/conv2010/software.html
+ *         -# The compilation flag <b>QUICK_EXIT</b> can be set to implement a quick version but
+ *            reduced accuracy option. See th readme. file for details.
+ *         -# For more information, see the iers2010.hpp file at the root directory. 
+ * 
+ * @attention The FORTRAN subroutines may ba updated; see that their C++ translations stay
+ *            updated too.
+ * 
+ * @cite      iers2010
+ *
+ * @copyright No COPYRIGHT whatsoever.
+ * 
+ * <b><center><hr>
+ * National Technical University of Athens <br>
+ *      Dionysos Satellite Observatory     <br>
+ *        Higher Geodesy Laboratory        <br>
+ *      http://dionysos.survey.ntua.gr
+ * </center></b>
+ */ 
+
+# include <numeric>
+# include <cmath>
+
+# ifdef USE_EXTERNAL_CONSTS
+  #include "gencon.hpp"
+# endif
+
+namespace iers2010 {
+  
+  namespace dtel {
+
+    void st1idiu (const double*,const double*,const double*,const double&,const double&,double*);
+      
+    void st1isem (const double*,const double*,const double*,const double&,const double&,double*);
+
+    void st1l1 (const double*,const double*,const double*,const double&,const double&,double*);
+      
+    void step2diu (const double*,const double&,const double&,double*);
+
+    void step2lon (const double*,const double&,double*);
+
+    /**
+    * @brief function to compute the scalar product of two vectors and their norms.
+    * @param[in]  x  Vector of dimension 3
+    * @param[in]  y  Vector of dimension 3
+    * @param[out] r1 (Euclidean) norm of vector x
+    * @param[out] r2 (Euclidean) norm of vector y
+    * @return        scalar product of vectors x and y
+    */
+    inline double _sprod_ (const double* x,const double* y,double& r1,double& r2) {
+      r1 = ::sqrt ( std::inner_product (x,x+3,x,.0e0) );
+      r2 = ::sqrt ( std::inner_product (y,y+3,y,.0e0) );
+      return std::inner_product (x,x+3,y,.0e0);
+    }
+ 
+  };
+  
+};
+
+#endif
