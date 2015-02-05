56d10adad8a55474d4b3e9d078208bfcfa216687
xanthos@ntua <xanthos@mail.ntua.gr>
2015-02-05 17:21:15 +0200

    added alternatives

---
+++ alternatives/compile.sh
@@ -0,0 +1,3 @@
+g++ -Wall -c -std=c++11 shclass.cpp
+g++ -Wall -c -std=c++11 gpt.cpp
+g++ -Wall -std=c++11 main.cpp gpt.o shclass.o
---
+++ alternatives/main.cpp
@@ -0,0 +1,41 @@
+#include "shclass.hpp"
+#include <ctime>
+
+int main ()
+{
+  double d, grn, gre;
+  std::clock_t start;
+  double dmjd;
+  
+  printf ("\nOne call to each function costs:");
+  dmjd = 55055e0;
+  
+  start = std::clock();
+  iers2010::gpt (dmjd,0.6708665767e0,-1.393397187e0,812.546e0,d,grn,gre);
+  printf ("\n\tTime for gpt v1 was %15.5f ms.", (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+  
+  start = std::clock();
+  iers2010::gpt2 (dmjd,0.6708665767e0,-1.393397187e0,812.546e0,d,grn,gre);
+  printf ("\n\tTime for gpt v2 was %15.5f ms.", (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+  
+  int calls = 1000;
+  printf ("\nMultiple (%5i) to each function cost:", calls);
+  dmjd = 55055e0;
+  start = std::clock();
+  for (int i = 0;i<calls;i++) {
+    iers2010::gpt (dmjd,0.6708665767e0,-1.393397187e0,812.546e0,d,grn,gre);
+    dmjd += (i/1e03);
+  }
+  printf ("\n\tTime for gpt v1 was %15.5f ms.", (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+  
+  dmjd = 55055e0;
+  start = std::clock();
+  for (int i = 0;i<calls;i++) {
+    iers2010::gpt2 (dmjd,0.6708665767e0,-1.393397187e0,812.546e0,d,grn,gre);
+    dmjd += (i/1e03);
+   }
+  printf ("\n\tTime for gpt v2 was %15.5f ms.", (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+
+  printf ("\n\n");
+  return 0;
+}
