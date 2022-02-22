352265d76cbbcb4d447fe5536f4966e329439140
xanthos <xanthos@mail.ntua.gr>
2022-02-22 15:06:27 +0200

    added +operator for Vec

--- src/matvec.hpp
+++ src/matvec.hpp
@@ -64,6 +64,11 @@ struct Vector3 {
 
 };
 
+inline
+Vector3 operator*(double scalar, const Vector3& vec) noexcept {
+  return vec * scalar;
+}
+
 struct Mat3x3 {
   double data[9] = {1e0, 0e0, 0e0, 0e0, 1e0, 0e0, 0e0, 0e0, 1e0};
 
