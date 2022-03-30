92910c0e6f906d411038d2d1949048a2918606dc
xanthos <xanthos@mail.ntua.gr>
2022-03-30 13:54:13 +0300

    added operator==for vectors

--- src/matvec.hpp
+++ src/matvec.hpp
@@ -67,6 +67,15 @@ struct Vector3 {
     data[2]+=v.data[2];
     return *this;
   }
+  constexpr Vector3& operator+=(double scalar) noexcept {
+    data[0]+=scalar;
+    data[1]+=scalar;
+    data[2]+=scalar;
+    return *this;
+  }
+  constexpr Vector3 operator+(double scalar) const noexcept {
+    return Vector3{{data[0]+scalar, data[1]+scalar, data[2]+scalar}};
+  }
   constexpr Vector3& operator-=(const Vector3 &v) noexcept {
     data[0]-=v.data[0];
     data[1]-=v.data[1];
@@ -93,7 +102,7 @@ Vector3 operator*(double scalar, const Vector3& vec) noexcept {
 }
 inline
 Vector3 operator+(double scalar, const Vector3& vec) noexcept {
-  return vec + scalar;
+  return vec+scalar;
 }
 
 
