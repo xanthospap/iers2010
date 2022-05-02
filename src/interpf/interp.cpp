9b374643471c165fbf2a334e505282176d9991fd
xanthos <xanthos.papanikolaou@gmail.com>
2022-05-02 19:38:22 +0300

    added readme for iau

--- src/interpf/interp.cpp
+++ src/interpf/interp.cpp
@@ -1,5 +1,5 @@
 #include "iers2010.hpp"
-//#include <cstdio>
+#include <cstdio>
 
 int iers2010::interp_pole(const double *mjd, const double *x, const double *y,
                           const double *ut1, int n, double rjd, double &xint,
@@ -12,7 +12,7 @@ int iers2010::interp_pole(const double *mjd, const double *x, const double *y,
   status = iers2010::interp::lagint(mjd, x, n, rjd, xint, index);
   status = iers2010::interp::lagint(mjd, y, n, rjd, yint, index);
   status = iers2010::interp::lagint(mjd, ut1, n, rjd, ut1int, index);
-  // printf("\tNote: Interpolated values: %+10.2f %+10.2f %+10.2f\n", xint, yint, ut1int);
+  printf("\tNote: Interpolated values: %+10.2f %+10.2f %+10.2f\n", xint, yint, ut1int);
 
   // corrections
   double corx, cory, corut1, corlod;
@@ -25,13 +25,13 @@ int iers2010::interp_pole(const double *mjd, const double *x, const double *y,
   xint += (corx * 1e-3);
   yint += (cory * 1e-3);
   ut1int += (corut1 * 1e-3);
-  // printf("\tNote: Oceanic Effect     : %+10.2f %+10.2f %+10.2f\n", corx*1e-3, cory*1e-3, corut1);
+  printf("\tNote: Oceanic Effect     : %+10.3f %+10.3f %+10.3f\n", corx*1e-3, cory*1e-3, corut1*1e-3);
 
   // lunisolar effect (results in [μas])
   iers2010::interp::pm_gravi(tjc, corx, cory);
   xint += (corx * 1e-3);
   yint += (cory * 1e-3);
-  // printf("\tNote: LuniSolar Effect   : %+10.2f %+10.2f %+10.2f\n", corx*1e-3, cory*1e-3, corut1);
+  printf("\tNote: LuniSolar Effect   : %+10.3f %+10.3f %+10.3f\n", corx*1e-3, cory*1e-3, 0e0);
 
   return status;
 }
